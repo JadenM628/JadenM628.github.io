1db091853c5cba22e552df4b148f424f341368fb
JadenM628 <marquezgaming628@gmail.com>
2022-11-30 15:36:54 -0500

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,6 +1,11 @@
 var date = new Date();
 
-window.onload = function() 
+function getTodaysDate()
 {
-    document.getElementById("date").innerHTML = "Today is " + date.toLocaleTimeString() + " on " + date.toDateString();
+    document.getElementById("date").innerHTML = "Today is " + date.toLocaleTimeString() + ", on " + date.toDateString();
+}
+
+
+function promptUser() {
+    document.getElementById("response").innerHTML = "The Jolly Machine welcomes you, " + document.getElementById("name").value + "! We're glad you're feeling " + document.getElementById("feeling").value + "!";
 }
